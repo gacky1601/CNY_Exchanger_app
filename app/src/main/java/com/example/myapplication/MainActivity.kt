c59a0cadb6d50861815edde7a96e32babd7b4798
gacky1601 <47267289+gacky1601@users.noreply.github.com>
2020-07-14 09:54:56 +0800

    implements timestamp retrieve.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.myapplication
 
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.text.format.DateFormat
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,19 +27,25 @@ class MainActivity : AppCompatActivity() {
         var paybaorate_local:Float = 4.275F
         var local_date=DateFormat.format("yyyy-MM-dd", Date())
         var db_date:String=""
-
+        var timestamp =DateTimeFormatter.ISO_INSTANT.format(Instant.now())
         //firebase
         var db = FirebaseFirestore.getInstance()
-        var dateRef=db.collection("last_update").document("date")
-        dateRef.get()
+        var dbpath=db.collection("exchange_rate")
+        var checkdate=dbpath.document("last_update")
+        val test_timestamp = 1594687935L
+        val triggerTime: LocalDateTime = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(test_timestamp),
+            TimeZone.getDefault().toZoneId()
+        )
+        checkdate.get()
             .addOnSuccessListener { document ->
                 db_date=document.getString("date").toString()
                 serverdate_tv.text=db_date.toString()
                 if(local_date!=db_date){
                     toast("伺服器尚未更新因此使用"+db_date+"之值")
                 }
-                var docRef=db.collection("exchange_rate").document(db_date.toString())
-                docRef.get()
+                var getexchangerate=dbpath.document(db_date.toString())
+                getexchangerate.get()
                     .addOnSuccessListener { document ->
                         twrate_local=document.getString("twbank_cny").toString().toFloat()
                         twexc_tv.text=twrate_local.toString()
